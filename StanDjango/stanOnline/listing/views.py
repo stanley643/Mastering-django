07f40281fb4ebf8f52e372ccab4bff027bac94cb
Stanley643 <nmurigi@kabarak.ac.ke>
2023-02-23 14:46:44 +0300

    views

--- StanDjango/stanOnline/listing/views.py
+++ StanDjango/stanOnline/listing/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Listing
 # Create your views here.
+#CRUD- create, retrieve, update, delete, list
+
+def listing_list(request):
+    listing= Listing.objects.all()
+    context ={
+        "listing": listing
+    }
+    return render(request, "listings.html", context)
